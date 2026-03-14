d6936459273881877709386ec75121984ed1b4d1
Ladladanish009 <125029069+Ladladanish009@users.noreply.github.com>
2026-03-15 01:07:46 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
 const settings = {
   packname: 'Arslan Tech Hub Bot',
   author: '‎',
-  botName: "A   T   H  Bot",
-  botOwner: 'ArslanMD Official', // Your name
+  botName: "𝐌𝐫_𝐃𝐚𝐧𝐢 🎩",
+  botOwner: '𝐌𝐫_𝐃𝐚𝐧𝐢 🎩', // Your name
   ownerNumber: '923237045919', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "👥public",
-  maxStoreMessages: 20, 
+  maxStoreMessages: 10000, 
   storeWriteInterval: 10000,
   description: "> ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ: ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ꜰᴏʀ ᴍᴀɴᴀɢɪɴɢ ɢʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅꜱ ᴀɴᴅ ᴀᴜᴛᴏᴍᴀᴛɪɴɢ ᴛᴀꜱᴋꜱ.",
-  caption: "𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐀𝐑𝐒𝐋𝐀𝐍-𝐌𝐃",
+  caption: "𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐌𝐫_𝐃𝐚𝐧𝐢 🎩",
   version: "2.2.6 alpha-8",
   prefix: ".", // prefix (e.g., ., /, !, *)
   timezone: "Asia/Karachi", //put your country timeZone....leave blank if u don't know
